35f116da4eb8c01643f223cc8085605bfa939869
hujun <jun.hu@linktronsys.com>
2020-05-31 14:13:31 +0800

    C# espeakTTS

---
+++ ConsoleApp1test/Program.cs
@@ -0,0 +1,31 @@
+﻿using ESpeakWrapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Client.Initialize("." );
+            Client.SetVoiceByName("zh+m10");
+            Client.Speak("helloworld！");
+            // 等待发音结束
+            Client.Espeak_Synchronize();
+            //Client.Espeak_Terminate();
+            //Thread.Sleep(5000);
+            Client.Stop();
+            Client.Speak("我爱，中国。Hello world");
+            Client.Espeak_Synchronize();
+            Client.Espeak_Terminate();
+            Console.Read();
+        }
+
+        
+    }
+}
